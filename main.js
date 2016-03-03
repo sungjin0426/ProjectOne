ae53a65d561364c02326c4be4e5b40a0da0b834a
sungjin0426 <sjlbest0426@gmail.com>
2016-03-03 14:32:25 -0500

    project

--- main.js
+++ main.js
@@ -5,25 +5,23 @@ window.onload = function() {
 };
 
 //questions for trivia
-var nbaTrivia = {
-  question: {
-    q1: "Current NBA commissioner is David Stern:", //false adam silver
-    q2: "Stephen Curry's jersey number is #30:", //true
-    q3: "Currently, LeBron James plays for Miami Heat:", //false Cleveland Cavaliers
-    q4: "The shortest NBA player was Muggsy Bogues:", //true
-    q5: "Shaquille O'Neal's shoe size is 23:", //true
-    q6: "All NBA teams are based in the USA:", //false Toronto Raptors
-    q7: "The tallest NBA player was 7 foot 6 Yao Ming:", //false 7'7'' Manute Bol and Gheorghe Mureşan
-    q8: "Stephen Curry's birthname is Wardell Stephen Curry II:", //true
-    q9: "Kobe Bryant's middle name is 'Bean':", //true
-    q10: "Michael Jordan is the only player to win three consecutive NBA Finals MVP Awards:", //false Shaquille O'Neal 2000-2002
+var nbaTrivia = [
+    { q1: "Current NBA commissioner is David Stern:", answer: 'Adam Silver, false' },
+    { q2: "Stephen Curry's jersey number is #30:", answer: true,},
+    { q3: "Currently, LeBron James plays for Miami Heat:", answer: "Cleveland Cavaliers, false" },
+    { q4: "The shortest NBA player was Muggsy Bogues:", answer: 'true'},
+    { q5: "Shaquille O'Neal's shoe size is 23:", answer: true },
+    { q6: "All NBA teams are based in the USA:", answer: "Toronto Raptors, false"},
+    { q7: "The tallest NBA player was 7 foot 6 Yao Ming:", answer:' 7foot 7inches Manute Bol and Gheorghe Mureşan, false'},
+    { q8: "Stephen Curry's birthname is Wardell Stephen Curry II:", answer: true },
+    { q9: "Kobe Bryant's middle name is 'Bean':", answer: true },
+    { q10: "Michael Jordan is the only player to win three consecutive NBA Finals MVP Awards:", answer: "Shaquille O'Neal 2000-2002, false"
   }
-};
+];
 var answer;
 var playerName;
-var game;
+var game = false;
 var currentPoint;
-var startButton = document.querySelector("#start");
 var truee = document.querySelector("#true");
 var falsee = document.querySelector("#false");
 var enterButton = document.querySelector("#enter");
@@ -33,6 +31,18 @@ var input = document.querySelector("#input");
 var question = document.querySelector(".question");
 var tab = document.querySelector(".question-tab");
 var resetButton = document.querySelector("#reset");
+
+//if start button is not clicked, don't start the trivia
+var startButton = document.querySelector("#start");
+startButton.addEventListener('click', function(){
+  console.log("start button is being clicked");
+  start();
+});
+
+function start() {
+  console.log('starting game');
+  game = true;
+}
 
 function reset() {
   resetButton.addEventListener('click', function(){
@@ -44,100 +54,90 @@ function createPlayer() {
 enterButton.addEventListener('click', function(){
 console.log("enter button is being clicked once");
 playerName = document.createElement('p');
-playerName.innerHTML = "Are you ready to play " + input.value +"?";
+playerName.innerHTML = "Are you ready to play, " + input.value +"?";
 userName.appendChild(playerName);
 });
 }
 
-//if start button is not clicked, don't start the trivia
-function startTrivia() {
-startButton.addEventListener('click', function(){
-  console.log("start button is being clicked");
-});
-}
-
-var left = truee.addEventListener('click', function() {
+var truthy = truee.addEventListener('click', function() {
   console.log("true is being clicked");
 });
 
-var right = falsee.addEventListener('click', function() {
+var falsy = falsee.addEventListener('click', function() {
   console.log("false is being clicked");
 });
+function question1() {
 
+}
 //question 1
 question.innerHTML = "Here is your first question.";
 var firstQ = document.createElement('p');
-firstQ.innerHTML = nbaTrivia.question.q1;
+firstQ.innerHTML = nbaTrivia[0].q1;
 question.appendChild(firstQ);
-answer = false;
-if (answer === left) {
-  console.log("wrong");
-} else if (answer === right) {
-    console.log("correct");
-  }
+
 //
 // //question 2
 // question.innerHTML = "Here is your second question";
 // var secondQ = document.createElement('p');
-// secondQ.innerHTML = nbaTrivia.question.q2;
+// secondQ.innerHTML = nbaTrivia[1].q2;
 // question.appendChild(secondQ);
 //
 // //question 3
 // question.innerHTML = "Here is your third question";
 // var thirdQ = document.createElement('p');
-// secondQ.innerHTML = nbaTrivia.question.q3;
+// secondQ.innerHTML = nbaTrivia[2].q3;
 // question.appendChild(thirdQ);
 //
 // //question 4
 // question.innerHTML = "Here is your fourth question. You are on a roll!";
 // var fourthQ = document.createElement('p');
-// fourthQ.innerHTML = nbaTrivia.question.q4;
+// fourthQ.innerHTML = nbaTrivia[3].q4;
 // question.appendChild(fourthQ);
 //
 // //question 5
 // question.innerHTML = "Four in a roll! Five more to go! So here...";
 // var fifthQ = document.createElement('p');
-// fifthQ.innerHTML = nbaTrivia.question.q5;
+// fifthQ.innerHTML = nbaTrivia[4].q5;
 // question.appendChild(fifthQ);
 //
 // //question 6
 // question.innerHTML = "Here is your sixth question.";
 // var sixthQ = document.createElement('p');
-// sixthQ.innerHTML = nbaTrivia.question.q6;
+// sixthQ.innerHTML = nbaTrivia[5].q6;
 // question.appendChild(sixthQ);
 //
 // //question 7
 // question.innerHTML = "You are good. But four more to go!";
 // var seventhQ = document.createElement('p');
-// seventhQ.innerHTML = nbaTrivia.question.q7;
+// seventhQ.innerHTML = nbaTrivia[6].q7;
 // question.appendChild(seventhQ);
 //
 // //question 8
 // question.innerHTML = "This is your eighth question. Three more to go!";
 // var eighthQ = document.createElement('p');
-// eighthQ.innerHTML = nbaTrivia.question.q8;
+// eighthQ.innerHTML = nbaTrivia[7].q8;
 // question.appendChild(eighthQ);
 //
 // //question 9
 // question.innerHTML = "Good job so Far. Here is your ninth question. Two more question to go!";
 // var ninthQ = document.createElement('p');
-// ninthQ.innerHTML = nbaTrivia.question.q9;
+// ninthQ.innerHTML = nbaTrivia[8].q9;
 // question.appendChild(ninthQ);
 //
 // //question 10
 // question.innerHTML = "WOW! You are almost there! One more question to go. So here is your Final question...";
 // var tenthQ = document.createElement('p');
-// tenthQ.innerHTML = nbaTrivia.question.q10;
+// tenthQ.innerHTML = nbaTrivia[9].q10;
 // question.appendChild(tenthQ);
 
 //Win
-var winner = document.createElement('div');
-winner.setAttribute('class', 'winner');
-winner.innerHTML = "You know your NBA. You Win!";
-tab.appendChild(winner);
+// var winner = document.createElement('div');
+// winner.setAttribute('class', 'winner');
+// winner.innerHTML = "You Win!!!";
+// tab.appendChild(winner);
 
 //Lose
-var loser = document.createElement('div');
-loser.setAttribute('class', 'loser');
-loser.innerHTML = "GAME OVER! You don't know your NBA.";
-tab.appendChild(loser);
+// var loser = document.createElement('div');
+// loser.setAttribute('class', 'loser');
+// loser.innerHTML = "GAME OVER! Try again next time.";
+// tab.appendChild(loser);
