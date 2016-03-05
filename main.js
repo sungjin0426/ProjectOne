ba01048995217f7a33b7c406e5d272c450d9eae4
sungjin0426 <sjlbest0426@gmail.com>
2016-03-05 17:21:45 -0500

    working on project

--- main.js
+++ main.js
@@ -3,42 +3,70 @@ window.onload = function() {
   hideButton();
   start();
   reset();
+  falsy();
+  truthy();
 };
 
-//list of questions
-var questions = [
-  "Current NBA commissioner is David Stern:",
-  "Stephen Curry's jersey number is #30:",
-  "Currently, LeBron James plays for Miami:",
-  "The shortest NBA player was Muggsy Bogues:",
-  "Shaquille O'Neal's shoe size is 23:",
-  "All NBA teams are based in the USA:",
-  "Stephen Curry's birthname is Wardell Stephen Curry II:",
-  "Kobe Bryant's middle name is 'Bean':",
-  "Michael Jordan is the only player to win three consecutive NBA Finals MVP Awards:"
-];
-
-//list of answers
-var checkAnswer = [
-  "False, Current NBA commissioner is Adam Silver.",
-  "True, Stephen Curry wear #30.",
-  "False, LeBron James plays for Cleveland Cavaliers.",
-  "True, Muggsy Bogues is 5 feet and 3 inches tall.",
-  "True, that's pretty big for human",
-  "False, Toronto Raptors is based in Canada",
-  "False, 7foot 7inches Manute Bol and Gheorghe Mureşan",
-  "True!",
-  "True, what a funny middle name ^^",
-  "False, Shaquille O'Neal won consecutively in season 2000-2002"
+//trivia questions, answers, explanation
+var data = [
+  {
+    question: "Current NBA commissioner is David Stern:",
+    answer: false,
+    explanation: "Current NBA commissioner is Adam Silver."
+  },
+  {
+    question: "Stephen Curry's jersey number is #30:",
+    answer: true,
+    explanation: "Stephen Curry wears #30."
+  },
+  {
+    question: "Currently, LeBron James plays for Miami:",
+    answer: false,
+    explanation: "LeBron James plays for Cleveland Cavaliers.",
+  },
+  {
+    question: "The shortest NBA player was Muggsy Bogues:",
+    answer: true,
+    explanation: "Muggsy Bogues is 5 feet and 3 inches tall.",
+  },
+  {
+    question: "Shaquille O'Neal's shoe size is 23:",
+    answer: true,
+    explanation: "That's pretty big for human.",
+  },
+  {
+    question: "All NBA teams are based in the USA:",
+    answer: false,
+    explanation: "Toronto Raptors is based in Canada.",
+  },
+  {
+    question: "The tallest NBA player was 7 foot 6 Yao Ming",
+    answer: false,
+    explanation: "Tallest player are (7foot 7inches) Manute Bol and Gheorghe Mureşan.",
+  },
+  {
+    question: "Stephen Curry's birthname is Wardell Stephen Curry II:",
+    answer: true,
+    explanation: "That's a cool name!",
+  },
+  {
+    question: "Kobe Bryant's middle name is 'Bean':",
+    answer: true,
+    explanation: "what a funny middle name!",
+  },
+  {
+    question: "Michael Jordan is the only player to win three consecutive NBA Finals MVP Awards:",
+    answer: false,
+    explanation: "Shaquille O'Neal won consecutively in season 2000-2002.",
+  }
 ];
 
 //when game is over
-var gameOverSay = ["GAMEOVER, please click RESET to try again."];
+var gameOverSay = "GAMEOVER, please click RESET to try again.";
 
 //when player beat the game
-var winnerSay = ["You Win! Play Again"];
+var winnerSay = "You Win! Please click RESET to Play Again";
 
-var triviaAnswer;
 //inital values by className
 var play = document.querySelector(".play");
 var pressStart = document.querySelector(".press");
@@ -48,7 +76,6 @@ var tab = document.querySelector(".questions");
 var goal = document.querySelector(".goal");
 var answer = document.querySelector(".answer");
 var enterName = document.querySelector(".enter-name");
-var ga = document.querySelector(".ga");
 
 //initial value by id
 var middle = document.querySelector("#middle");
@@ -64,22 +91,27 @@ var content = document.querySelector("#content");
 //winner
 function winner() {
   enterName.innerHTML = winnerSay;
-  resetButton.style.visibility = 'visible';
-}
-//remove all
-function removeAll() {
-  play.innerHTML = gameOverSay;
   goal.innerHTML = "";
   resetButton.style.visibility = 'visible';
   tab.removeChild(truee);
   tab.removeChild(falsee);
 }
+
+//remove all
+function removeAll() {
+  goal.innerHTML = "";
+  resetButton.style.visibility = 'visible';
+  tab.removeChild(truee);
+  tab.removeChild(falsee);
+}
+
 //hide buttons
 function hideButton() {
-truee.style.visibility = 'hidden';
-falsee.style.visibility = 'hidden';
-resetButton.style.visibility = 'hidden';
+  truee.style.visibility = 'hidden';
+  falsee.style.visibility = 'hidden';
+  resetButton.style.visibility = 'hidden';
 }
+
 //show button
 function showButton() {
   truee.style.visibility = 'visible';
@@ -87,55 +119,81 @@ function showButton() {
   resetButton.style.visibility = 'visible';
 }
 
+//start button
 function start() {
   enterButton.addEventListener('click', function(){
-  console.log("enter button is being clicked once");
-  play.innerHTML = input.value + ", Get all 10 questions right & You win!";
-  goal.innerHTML = "Here is your first question.";
-  middle.removeChild(enterName);
-  user.removeChild(input);
-  user.removeChild(enterButton);
-  showButton();
-  question.innerHTML = questions[0];
-});
+    console.log("enter button is being clicked once");
+    if (input.value === input.value){
+    play.innerHTML = input.value + ", Get all 10 questions right & You win!";
+    goal.innerHTML = "Here is your first question.";
+    middle.removeChild(enterName);
+    user.removeChild(input);
+    user.removeChild(enterButton);
+    showButton();
+    question.innerHTML = data[questionNumber].question;
+  } else if (input.value === false) {
+    play.innerHTML = "Please identify yourself!";
+    middle.removeChild(enterName);
+    user.removeChild(input);
+    user.removeChild(enterButton);
+  }
+  });
 }
 
+//reset
 function reset() {
   resetButton.addEventListener('click', function(){
   location.reload();
 });
 }
-var questionNumber = 1;
+
+//Question and Answer Buttons
+var questionNumber = 0;
 var answerNumber = 0;
-
-// //question 1, if false is clicked, the next question will appear. else if true is clicked, game is over.
-
-
-//show questions
-//question 1
 function falsy() {
   falsee.addEventListener('click', function(){
-  console.log("false is clicked.");
-});
-  if (triviaAnswer == checkAnswer[answerNumber]) {
-    questionNumber += 1;
-    answerNumber += 1;
-    questions.innerText = questions[questionsNumber];
-  } else {
-    question.innerHTML = "Correct answer was, " + checkAnswer[answerNumber];
-    removeAll();
-  }
+    var value = false;
+    console.log("false is clicked.");
+    console.log(data[questionNumber].answer);
+    if (value == data[questionNumber].answer) {
+      if (questionNumber === 9) {
+        //winner
+        winner();
+      } else {
+        //next question
+        questionNumber += 1;
+        goal.innerHTML = "";
+        play.innerHTML = "Question " + (questionNumber + 1) + " of 10";
+        question.innerHTML = data[questionNumber].question;
+      }
+    } else {
+      answer.innerHTML = data[questionNumber].explanation;
+      questionNumber = 0;
+      play.innerHTML = gameOverSay;
+      removeAll();
+    }
+  });
 }
+
 function truthy() {
   truee.addEventListener('click', function() {
+  value = true;
   console.log("true is clicked.");
-  if (triviaAnswer == checkAnswer[answerNumber]) {
-    questions.innerText = question[questionsNumber];
-    answer.innerHTML = "Correct!";
+  console.log(data[questionNumber].answer);
+  if (value == data[questionNumber].answer) {
+    if(questionNumber === 9) {
+      //winner
+      winner();
+    } else {
     questionNumber += 1;
-    answerNumber += 1;
+    goal.innerHTML = "";
+    play.innerHTML = "Question " + (questionNumber + 1) + " of 10";
+    question.innerHTML = data[questionNumber].question;
+    }
   } else {
-    question.innerHTML = "Correct answer was, " + checkAnswer[answerNumber];
+    answer.innerHTML = data[questionNumber].explanation;
+    questionNumber = 0;
+    play.innerHTML = gameOverSay;
     removeAll();
   }
 });
